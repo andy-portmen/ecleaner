6523848358a18a1427724a31a300890397754a64
Andy Portmen <andy.portmen@gmail.com>
2017-04-24 15:15:48 +0430

    options are now persistent

--- common.js
+++ common.js
@@ -1,14 +1,22 @@
 'use strict';
 
-chrome.storage.local.get('version', (obj) => {
+// FAQs & Feedback
+chrome.storage.local.get({
+  'version': null,
+  'faqs': navigator.userAgent.toLowerCase().indexOf('firefox') === -1 ? true : false
+}, prefs => {
   let version = chrome.runtime.getManifest().version;
-  if (obj.version !== version) {
-    window.setTimeout(() => {
-      chrome.storage.local.set({version}, () => {
-        chrome.tabs.create({
-          url: 'http://add0n.com/ecleaner.html?version=' + version + '&type=' + (obj.version ? ('upgrade&p=' + obj.version) : 'install')
-        });
+
+  if (prefs.version ? (prefs.faqs && prefs.version !== version) : true) {
+    chrome.storage.local.set({version}, () => {
+      chrome.tabs.create({
+        url: 'http://add0n.com/ecleaner.html?version=' + version +
+          '&type=' + (prefs.version ? ('upgrade&p=' + prefs.version) : 'install')
       });
-    }, 3000);
+    });
   }
 });
+(function () {
+  let {name, version} = chrome.runtime.getManifest();
+  chrome.runtime.setUninstallURL('http://add0n.com/feedback.html?name=' + name + '&version=' + version);
+})();
--- data/panel/index.js
+++ data/panel/index.js
@@ -1,20 +1,35 @@
 'use strict';
 
-document.getElementById('submit').addEventListener('click', () => {
+document.getElementById('submit').addEventListener('click', (e) => {
+  e.target.value = 'Please wait ...'
+  e.target.disabled = true;
   let since = +document.getElementById('since').querySelector(':checked').value;
-  let types = Array.from(document.getElementById('types').querySelectorAll(':checked')).map(e => e.value);
-  let zones = Array.from(document.getElementById('zones').querySelectorAll(':checked')).map(e => e.value);
+  let types = [...document.getElementById('types').querySelectorAll(':checked')].map(e => e.value);
+  if (navigator.userAgent.indexOf('Firefox') !== -1) {
+    types = types.filter(t => ['appcache', 'fileSystems', 'webSQL'].indexOf(t) === -1);
+  }
+  let zones = [...document.getElementById('zones').querySelectorAll(':checked')].map(e => e.value);
+  // persist
+  localStorage.setItem('since', since);
+
   chrome.browsingData.remove({
     'since': since ? (new Date()).getTime() - since * 1000 : since,
     'originTypes': zones.reduce((p,c) => {p[c] = true; return p;}, {})
   }, types.reduce((p,c) => {p[c] = true; return p;}, {}), () => {
-    //window.close();
+    window.setTimeout(() => {
+      window.close();
+    }, 1000);
   });
 });
 var last;
 
 document.body.addEventListener('change', (e) => {
   let target = e.target;
+  // persist
+  if (target.type === 'checkbox') {
+    localStorage.setItem(target.value, target.checked);
+  }
+  //
   let confirm = target.dataset.confirm;
   if (confirm && target.checked) {
     last = target;
@@ -36,3 +51,16 @@ document.body.addEventListener('click', (e) => {
     confirm.style.display = 'none';
   }
 });
+
+// persist
+[...document.querySelectorAll('[type=checkbox]')].forEach(checkbox => {
+  let value = localStorage.getItem(checkbox.value);
+  if (value !== null && checkbox.dataset.confirm !== 'true') {
+    checkbox.checked = value === 'true';
+  }
+});
+(function (since) {
+  if (since && since !== '0') {
+    document.getElementById('since').querySelector(`[value="${since}"]`).checked = true;
+  }
+})(localStorage.getItem('since'));
